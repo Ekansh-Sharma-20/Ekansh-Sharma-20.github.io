f6471d1105e1c72a3bb442ad093e7b2ad0137d80
Ekansh-Sharma-20 <ekanshsharma2002@outlook.com>
2020-10-06 18:38:49 +0530

    Final Revision

---
+++ stylesheets/chatfuel.js
@@ -0,0 +1,29 @@
+var div = document.createElement('div');
+        div.className = 'fb-customerchat';
+        div.setAttribute('page_id', '108433304363181');
+        div.setAttribute('ref', 'b64:bGl2ZS1jaGF0');
+        document.body.appendChild(div);
+        window.fbMessengerPlugins = window.fbMessengerPlugins || {
+          init: function () {
+            FB.init({
+              appId            : '1678638095724206',
+              autoLogAppEvents : true,
+              xfbml            : true,
+              version          : 'v7.0'
+            });
+          }, callable: []
+        };
+        window.fbAsyncInit = window.fbAsyncInit || function () {
+          window.fbMessengerPlugins.callable.forEach(function (item) { item(); });
+          window.fbMessengerPlugins.init();
+        };
+        setTimeout(function () {
+          (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) { return; }
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+            fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));
+        }, 0);
